a0ca9bf0ca535b06b2bbdc2df8c4db2264891410
BAGIRADHAN S <bagiradhansrinivasan@gmail.com>
2026-06-08 22:07:05 +0530

    Create files.f

---
+++ run/files.f
@@ -0,0 +1,15 @@
++incdir+/home/dvft1104/axi_4Lite/verification/agent/
++incdir+/home/dvft1104/axi_4Lite/verification/env/
++incdir+/home/dvft1104/axi_4Lite/verification/test/
++incdir+/home/dvft1104/axi_4Lite/verification/tb/
+
+
+/home/dvft1104/axi_4Lite/verification/tb/axi_interface.sv
+
+/home/dvft1104/axi_4Lite/verification/agent/axi_agent_package.svp
+/home/dvft1104/axi_4Lite/verification/env/axi_env_package.svp
+/home/dvft1104/axi_4Lite/verification/test/axi_test_package.svp
+
+/home/dvft1104/axi_4Lite/verification/tb/axi_assertion.sv
+/home/dvft1104/axi_4Lite/design/design.sv
+/home/dvft1104/axi_4Lite/verification/tb/axi_top.sv
